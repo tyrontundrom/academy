0b7ee12281cc0a159557832d14a6f91da4ff3cc4
Piotr Lyszkowski <tyrontundrom@gmail.com>
2022-11-21 22:25:18 +0100

    validate

--- src/main/java/com/example/academy/domain/Student.java
+++ src/main/java/com/example/academy/domain/Student.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,9 +20,12 @@ public class Student {
     @Id
     @GeneratedValue
     private Long id;
+    @Size(min = 3, message = "imię musi być dłuższe niż dwie litery")
     private String firstName;
     private String lastName;
+    @Min(value = 18, message = "wiek minimalny to 18 lat")
     private Integer age;
+    @Email(message = "nie poprawny format adresu email")
     private String email;
     private String subject;
 
--- src/main/java/com/example/academy/domain/Teacher.java
+++ src/main/java/com/example/academy/domain/Teacher.java
@@ -3,8 +3,13 @@ package com.example.academy.domain;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,9 +22,12 @@ public class Teacher {
     @Id
     @GeneratedValue
     private Long id;
+    @Size(min = 3, message = "imię musi być dłuższe niż dwie litery")
     private String firstName;
     private String lastName;
+    @Min(value = 18, message = "wiek minimalny to 18 lat")
     private Integer age;
+    @Email(message = "nie poprawny format adresu email")
     private String email;
     private String subject;
 
